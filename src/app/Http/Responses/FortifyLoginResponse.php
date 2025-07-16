1c0b650c5d23bda8119218ea09aaaa704ee55914
hisashi mamada <rm8422gitabero@gmail.com>
2025-07-16 21:45:13 +0900

    メール認証機能の実装

--- src/app/Http/Responses/FortifyLoginResponse.php
+++ src/app/Http/Responses/FortifyLoginResponse.php
@@ -6,14 +6,21 @@ use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 
 class FortifyLoginResponse implements LoginResponseContract
 {
-    public function toResponse($request)
+    public function toResponse($request): \Illuminate\Http\RedirectResponse
     {
         $user = $request->user();
 
+        // メール未認証なら認証画面へ
+        if (!$user->hasVerifiedEmail()) {
+            return redirect()->route('verification.notice');
+        }
+
+        // ロールでルート振り分け
         if ($user->role === 'admin') {
             return redirect()->route('admin.attendances.index');
         }
 
+        // 一般ユーザーの場合
         return redirect()->route('attendances.index');
     }
 }
